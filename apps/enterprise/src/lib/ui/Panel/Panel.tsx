fb5462eee96a48013faf6f306e14c2cd9be2ef1d
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-10-20 01:01:10 +0800

    Update Panel.tsx

--- apps/enterprise/src/lib/ui/Panel/Panel.tsx
+++ apps/enterprise/src/lib/ui/Panel/Panel.tsx
@@ -25,7 +25,7 @@ export const Panel = styled.div<PanelProps>`
   overflow: hidden;
 
   ${({ withSections, kind }) => {
-    const contentBackground = kind === 'secondary' ? 'linear-gradient(-45deg, #000000 0%, #40c9ff 100%)' : 'linear-gradient(-45deg, #000000 0%, #00dbde 100%)';
+    const contentBackground = kind === 'secondary' ? 'linear-gradient(-45deg, #000000 0%, #121240 100%)' : 'linear-gradient(-45deg, #000000 0%, #00dbde 100%)';
     const border = kind === 'secondary' ? '2px solid #e81cff' : '';
 
     return css`
